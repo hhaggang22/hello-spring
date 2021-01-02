8135b8a8e05ce956685b8016479975b0126d26ab
hhaggang22 <20181018@sungshin.ac.kr>
2021-01-03 06:07:47 +0900

    정적 컨텐츠, MCV 템플릿 실습

--- hello-spring/src/main/java/com/phinochhio/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/com/phinochhio/hellospring/controller/HelloController.java
@@ -3,12 +3,19 @@ package com.phinochhio.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello") //html에서 hello주소로 가면 이쪽으로 넘어옴
     public String hello(Model model){
         model.addAttribute("data", "hello!");
-        return "hello";
+        return "hello"; //return 의 이름이 hello => templates에 hello라는 이름을 가진 파일을 viewResolver가 찾아서 가라
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){ //값을 받아올 때는 RequestParam
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
